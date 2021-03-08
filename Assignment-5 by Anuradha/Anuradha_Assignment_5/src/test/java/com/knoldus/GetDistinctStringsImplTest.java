4d08fde5e985ba7c1755d33e277cc25c5ceba243
AnuradhaKumari201 <anu@codewithanu.com>
2021-03-08 23:55:11 +0530

    Anuradha's Assignment-s

---
+++ Assignment-5 by Anuradha/Anuradha_Assignment_5/src/test/java/com/knoldus/GetDistinctStringsImplTest.java
@@ -0,0 +1,25 @@
+package com.knoldus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.*;
+
+class GetDistinctStringsImplTest {
+    private static final Object Uniqueset = null;
+
+    @Test
+    public void uniqueStringTest(){
+        GetDistinctStringsImpl obj1 = new GetDistinctStringsImpl();
+        List<String> Uniqueset = obj1.get.distinctStrings(new ArrayList<>(Arrays.asList("java","java","c")));
+        List<String> strList= new ArrayList<>();
+        strList.add("c");
+        strList.add("java");
+
+        Assertions.assertEquals(strList,Uniqueset);
+    }
+
+  
+    }
+
